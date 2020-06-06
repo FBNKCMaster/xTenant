3bd9f6a7a175587998b144d67b9617637b034c34
FBNKCMaster <FBNKCMaster@gmail.com>
2020-06-06 21:49:05 +0100

    Change the way to handle assets

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,14 @@
 <?php
 
 use FBNKCMaster\xTenant\Models\XTenantParam;
-use Illuminate\Http\RedirectResponse;
 
 $domain = XTenantParam::getDomain();
 $superAdminSubdomain = XTenantParam::getSuperAdminSubdomain();
 
-Route::domain('{subdomain}.' . $domain)->group(function () use ($superAdminSubdomain) {
+Route::domain($superAdminSubdomain . '.' . $domain)->group(function () {
 
-    // Redirect static assets
-    Route::get('{any}', function ($subdomain, $any) {
-        $extensions = [
-            'jpg', 'jpeg', 'png', 'gif', 'svg',
-            'mp4', 'mov', 'webm', 'avi', 'mkv',
-            'css',
-            'js',
-            'pdf',
-        ];
-        if (preg_match('(' . implode('|', $extensions) .')', $any)) {
-            $host = request()->getHost();
-            $newPath = str_replace($host, $host . '/' . $subdomain . '/public', url()->current());
-            
-            return new RedirectResponse($newPath, 302, [
-                'Cache-Control' => 'public, max-age=3600',
-            ]);
-        }
-    
-    })->where([
-        'subdomain' => '^((?!' . $superAdminSubdomain . ').)*$',
-        'any'       => '^(\w+/){1,2}\w+\.\w+$'
-    ]);
+    Route::get('{any}', function () {
+        return 'This SuperAdmin Area';
+    })->where('any', '.*');
 
 });
