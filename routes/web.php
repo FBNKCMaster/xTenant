816a9e9a2bfd905ed64d415c4911cb7469d736c9
FBNKCMaster <FBNKCMaster@gmail.com>
2020-06-16 17:15:13 +0100

    wip & clean up

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,6 @@ $controllersNamespace = 'FBNKCMaster\xTenant\Controllers\\';
 
 Route::domain($superAdminSubdomain . '.' . $domain)->group(function () use ($controllersNamespace) {
 
-    /* Route::get('{any}', function () {
-        return 'This SuperAdmin Area';
-    })->where('any', '.*'); */
-
-    /* Route::get('/', function () {
-        return view('xtenant::dashboard');
-        //return view('xtenant::howto');
-    }); */
-
-    // Authentication Routes...
-    //$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    //$this->post('login', 'Auth\LoginController@login');
-    //$this->post('logout', 'Auth\LoginController@logout')->name('logout');
     Route::group(['middleware' => ['web']], function () use ($controllersNamespace) {
         
         Route::get('/login', $controllersNamespace . 'LoginController@showLoginForm')->name('login');
@@ -29,10 +16,6 @@ Route::domain($superAdminSubdomain . '.' . $domain)->group(function () use ($con
         Route::post('/logout', $controllersNamespace . 'LoginController@logout')->name('logout');
         Route::get('/register', function () { return redirect('/'); })->name('register');
 
-    /* });
-
-    Route::group(['middleware' => ['web', 'auth:superadmin']], function () use ($controllersNamespace) { */
-        //routes should go here
         Route::get('/dashboard', $controllersNamespace . 'SuperAdminController@dashboard');
         Route::get('/settings', $controllersNamespace . 'SuperAdminController@settings');
         Route::patch('/settings', $controllersNamespace . 'SuperAdminController@update_settings')->name('settings');
